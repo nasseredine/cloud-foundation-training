99d88bb984ba5095b4870f89ccb5eea9506abfd6
Nasseredine Bajwa <fr.bajwa.nasseredine@gmail.com>
2024-01-29 14:51:24 +0100

    06-Cloud-Function done

--- 06-Cloud-Function/main.tf
+++ 06-Cloud-Function/main.tf
@@ -35,7 +35,6 @@ resource "random_id" "suffix" {
  * - source: "terraform-google-modules/event-function/google"
  * - version: "~> 2.2.0"
  * - name: "lab06-cloud-function-${var.project_id}-${random_id.suffix.hex}"
- * - project_id: module.project_iam_bindings.projects[0]
  * - region: var.region (https://cloud.google.com/functions/docs/locations)
  * - description: "Process image in GCS bucket"
  * - entry_point: "blur_images"
@@ -57,5 +56,22 @@ resource "random_id" "suffix" {
  *
  */
 module "image_processing_function" {
-
+  source                = "terraform-google-modules/event-function/google"
+  version               = "~> 2.2.0"
+  name                  = "lab06-cloud-function-${var.project_id}-${random_id.suffix.hex}"
+  project_id            = var.project_id
+  region                = var.region
+  description           = "Process image in GCS bucket"
+  entry_point           = "blur_images"
+  runtime               = "python37"
+  source_directory      = "${path.module}/function_source"
+  bucket_force_destroy  = true
+  service_account_email = google_service_account.image_processing_gcf_sa.email
+  environment_variables = {
+    "BLURRED_BUCKET_NAME" = google_storage_bucket.image_processed.name
+  }
+  event_trigger = {
+    "event_type" = "google.storage.object.finalize"
+    "resource"   = google_storage_bucket.image_upload.name
+  }
 }
